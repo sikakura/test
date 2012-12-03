aa585f641ea3a260676cd4934638780c58eb7ba5
Naoto Shikakura <sikakura@gmail.com>
2012-12-03 17:22:03 +0900

    ruby on railsのテスト（BLOGサイト） http://dqn.sakusakutto.jp/2012/03/rails32blog.html の通りに実施。

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6a3a382f33a4e02cdd52b7327c2d6a1966b5e37a1c1f93924e2c8441b0ad8bf59e17095c3a472ee1e7e0b74e34516db75354d0a6ab8a4626102a536756748900'
